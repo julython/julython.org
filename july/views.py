015e025b608539eb7f858fca04ede3cda0c1a333
Robert Myers <robert@keyingredient.com>
2012-04-28 17:08:49 -0500

    Adding sections to the index view

--- july/views.py
+++ july/views.py
@@ -3,7 +3,14 @@ from django.views.decorators.cache import cache_page
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from july.pages.models import Section
+
 @cache_page(60)
 def index(request):
-    """Render the home page"""   
-    return render_to_response('index.html', RequestContext(request, {}))
+    """Render the home page"""
+    
+    sections = Section.all().order('order')
+    
+    return render_to_response('index.html', RequestContext(request, {
+        'sections': sections,
+    }))
