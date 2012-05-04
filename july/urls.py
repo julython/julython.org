7d6022141323655de9d68225b7c6e6a5cd1c16dd
Robert Myers <robert@keyingredient.com>
2012-05-03 22:55:52 -0500

    Adding registration templates and hooking up apps

--- july/urls.py
+++ july/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.auth import views
 from gae_django import admin
+from gae_django.auth.forms import AuthenticationForm
 
 admin.autodiscover()
 
@@ -7,5 +9,8 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'july.views.index', name='index'),
+    url(r'^signin/$', views.login, {'authentication_form': AuthenticationForm}),
+    url(r'^signout/$', views.logout),
     url(r'^accounts/', include('gae_django.auth.urls')),
+    url(r'^me/', include('july.people.urls')),
 )
