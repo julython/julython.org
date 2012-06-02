06ef0b92b1c8e6cc2916f4d68ec3b4ae513c9085
Robert Myers <robert@keyingredient.com>
2012-06-01 22:41:12 -0500

    Fix typo and move missing import into edit view

--- july/people/views.py
+++ july/people/views.py
@@ -5,7 +5,6 @@ from django.template.context import RequestContext
 from july.people.models import Commit
 from gae_django.auth.models import User
 from django.http import Http404
-from forms import EditUserForm
 
 def user_profile(request, username):
     user = User.all().filter("username", username).get()
@@ -19,12 +18,13 @@ def user_profile(request, username):
  
 @login_required
 def edit_profile(request, username, template_name='people/edit.html'):
+    from forms import EditUserForm
     user = request.user
     #CONSIDER FILES with no POST?  Can that happen?
     form = EditUserForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         for key in form.cleaned_data:
-           setattr(user,key,form.cleaned_data.get(key))
+            setattr(user,key,form.cleaned_data.get(key))
         user.put()
     
     if user == None:
