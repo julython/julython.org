c465afa868f5c92f16656e6ee85419e71509239e
Robert Myers <robert@keyingredient.com>
2012-06-15 08:48:24 -0500

    Adding a less_compile command for fabric

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,12 @@
 """
 Common tools for deploy run shell and the like.
 """
+from fabric.api import lcd
 
 from gae_django.fabric_commands import *
+
+@task
+def less_compile(compress=True):
+    """Compile the .less files into css."""
+    with lcd('july'):
+        local("lessc --compress static_root/less/layout.less > static_root/css/main.css")
