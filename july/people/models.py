aa74413146d65b9feb7e3cc150d0357c93a4c9c8
Robert Myers <robert@keyingredient.com>
2012-06-19 23:10:08 -0500

    Adding helper methods

--- july/people/models.py
+++ july/people/models.py
@@ -2,6 +2,7 @@ import logging
 from urlparse import urlparse
 
 from google.appengine.ext import ndb
+from gae_django.auth.models import User
 
 class Commit(ndb.Model):
     """
@@ -17,6 +18,42 @@ class Commit(ndb.Model):
     url = ndb.StringProperty()
     timestamp = ndb.StringProperty()
     created_on = ndb.DateTimeProperty(auto_now_add=True)
+    
+    @classmethod
+    def create_by_email(cls, email, data):
+        """Create a commit by email address"""
+        return cls.create_by_auth_id('email:%s' % email, data)
+    
+    @classmethod
+    def create_by_auth_id(cls, auth_id, data):
+        user = User.get_by_auth_id(auth_id)
+        if user:
+            return cls.create_by_user(user, data)
+        return cls.create_orphan(data)
+    
+    @classmethod
+    def create_by_user(cls, user, data):
+        """Create a commit with parent user, updating users points."""
+        user_key = user.key
+        
+        @ndb.transactional
+        def txn():
+            user = user_key.get()
+            commit = Commit(parent=user_key, **data)
+            count = getattr(user, 'total', 0)
+            user.total = count + 1
+            ndb.put_multi([commit, user])
+            return commit
+        
+        commit = txn()
+        return commit
+    
+    @classmethod
+    def create_orphan(cls, data):
+        """Create a commit with no parent."""
+        commit = Commit(**data)
+        commit.put()
+        return commit
     
 class Project(ndb.Model):
     """
@@ -47,15 +84,26 @@ class Project(ndb.Model):
             Given:
               http://github.com/julython/julython.org
             Return:
-              julython-julython-org
+              gh-julython-julython.org
         
         This is used as the Key name in order to speed lookups during
         api requests.
         """
-        logging.debug("Parsing url: %s", url)
+        hosts_lookup = {
+            'github.com': 'gh',
+            'bitbucket.org': 'bb',
+        }
         parsed = urlparse(url)
         path = parsed.path
         if path.startswith('/'):
             path = path[1:]
         tokens = path.split('/')
-        return '-'.join(tokens)
+        host_abbr = hosts_lookup.get(parsed.netloc, 'o')
+        name = '-'.join(tokens)
+        return '%s-%s' % (host_abbr, name)
+    
+    @classmethod
+    def make_key(cls, url):
+        """Return a ndb.Key from a url."""
+        name = cls.parse_project_name(url)
+        return ndb.Key('Project', name)
