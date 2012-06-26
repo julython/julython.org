0943ce5eabe874c3b26375e061a423199303570e
Kevin Mooney <kevin@cukerinteractive.com>
2012-06-26 00:31:19 -0500

    Added url for location view

--- july/people/urls.py
+++ july/people/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('july.people.views',
+    url(r'^location/$', 'locations', name='edit-profile'),
     url(r'^location/(?P<location_slug>[a-zA-Z0-9\-]+)/$', 'users_by_location', name='edit-profile'),
     url(r'^(?P<username>[a-zA-Z0-9_]+)$', 'user_profile', name='member-profile'),
     url(r'^(?P<username>[a-zA-Z0-9_]+)/edit/$', 'edit_profile', name='edit-profile'),
