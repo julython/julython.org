e784424183bdebc6d7c9ec0b7d2a781776ae710e
Josh Marshall <catchjosh@gmail.com>
2012-07-14 15:53:12 -0500

    Adding first version of Channel JS library.

---
+++ july/static_root/js/channel.js
@@ -0,0 +1,59 @@
+(function() {
+
+  var Channel = function(el, token, options) {
+    this._el = $(el);
+    this._options = options || {};
+    this._token = token;
+    this._setupOptions();
+    this._connect();
+  };
+
+  Channel.prototype._setupOptions = function() {
+    this._options.maxMessages = this._options.maxMessages || 100;
+  };
+
+  Channel.prototype._connect = function() {
+    // pretending to connect
+    console.log("We're connecting!");
+    this._startDevMessages();
+  };
+
+  Channel.prototype._newMessage = function(message) {
+    var messageView = this._buildMessageView(message);
+    messageView.hide();
+    this._el.prepend(messageView);
+    messageView.slideDown();
+    while (this._el.children().length > this._options.maxMessages) {
+      this._el.children().last().remove();
+    }
+  };
+
+  Channel.prototype._buildMessageView = function(message) {
+    // should move to a template eventually...
+    var li = $('<li class="message"></li>');
+    li.append('<img src="'+message.image+'" class="profile-image"/>');
+    li.append('<h4 class="username">'+message.user+'</h4>');
+    var p = $('<p class="message-content"></p>');
+    p.text(message.message);
+    li.append(p);
+    return li;
+  };
+
+  Channel.prototype._startDevMessages = function() {
+    var self = this;
+    this._interval = window.setInterval(function() {
+      self._newMessage({
+        user: "Joe User",
+        image: "/static/images/participating_button.png",
+        message: "This is a new message!"
+      });
+    }, 1200);
+  };
+
+  if (!window.julython) {
+    window.julython = {};
+  }
+
+  window.julython.Channel = Channel;
+
+})();
