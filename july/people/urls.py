cd4a2483a9069596682eb6c6f6e1abede77e2e5c
Kevin Mooney <kevin@cukerinteractive.com>
2012-11-17 18:16:54 -0600

    Added form for entering a winners address

--- july/people/urls.py
+++ july/people/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('july.people.views',
     url(r'^projects/(?P<slug>.+)/$', 'project_details', name='project-details'),
     url(r'^(?P<username>[a-zA-Z0-9_]+)/$', 'user_profile', name='member-profile'),
     url(r'^(?P<username>[a-zA-Z0-9_]+)/edit/$', 'edit_profile', name='edit-profile'),
+    url(r'^(?P<username>[a-zA-Z0-9_]+)/address/$', 'edit_address', name='edit-address'),
     url(r'^(?P<username>[a-zA-Z0-9_]+)/email/(?P<email>.*)$', 'delete_email', name='delete-email'),
     url(r'^(?P<username>[a-zA-Z0-9_]+)/projects/$', 'people_projects', name='user-projects'),
 )
