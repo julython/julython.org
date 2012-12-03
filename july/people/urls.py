d63b2588b10a4f0dbb627c929897edf81fbe58c1
Robert Myers <robert.myers@rackspace.com>
2012-12-03 09:16:01 -0600

    Fixing urls for django 1.5

--- july/people/urls.py
+++ july/people/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('july.people.views',
     url(r'^people/$', 'leaderboard', name='leaderboard'),
