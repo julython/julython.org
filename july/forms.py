f2676412276557b37358fd07bb133a99a961a23c
Roman Levin <roman.levin@stxnext.pl>
2013-06-11 15:10:41 +0200

    Moving user identification to email from username. Changes to the routes, views and models, and adding new forms. This is kind of hacky write now. We should switch to inheriting the User model from AbstractBaseUser.

---
+++ july/forms.py
@@ -0,0 +1,50 @@
+from july.models import User
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+
+class UserCreationForm(forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
+    error_messages = {
+        'duplicate_username': _("A user with that username already exists."),
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+    email = forms.EmailField()
+    password1 = forms.CharField(
+        label=_("Password"),
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput,
+        help_text=_("Enter the same password as above, for verification."))
+
+    class Meta:
+        model = User
+        fields = ("email",)
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        try:
+            User._default_manager.get(email=email)
+        except User.DoesNotExist:
+            return email
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'])
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.username = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
--- july/views.py
+++ july/views.py
@@ -1,19 +1,20 @@
 import json
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render, redirect
 from django.template import Context
 from django.conf import settings
 from django.http import HttpResponseRedirect
 
 from july.game.models import Game
+from july.forms import UserCreationForm
 
 
 def index(request):
     """Render the home page"""
     game = Game.active_or_latest()
     stats = game.histogram
-    
+
     ctx = Context({
         'stats': json.dumps(stats),
         'game': game,
@@ -21,38 +22,60 @@ def index(request):
         'user': request.user,
         'MEDIA_URL': settings.MEDIA_URL,
         'STATIC_URL': settings.STATIC_URL})
-    
+
     return render_to_response('index.html', context_instance=ctx)
 
+
 def help_view(request):
-    """Render the help page"""       
+    """Render the help page"""
     ctx = Context({
         'user': request.user,
         'MEDIA_URL': settings.MEDIA_URL,
         'STATIC_URL': settings.STATIC_URL})
-    
+
     return render_to_response('help.html', context_instance=ctx)
+
+
+def register(request):
+    """Register a new user"""
+    form = UserCreationForm()
+    if request.POST:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # Save user
+            user = form.save(commit=False)
+            user.save()
+            return redirect('july.views.index')
+    else:
+        form = UserCreationForm()
+
+    return render(
+        request,
+        'registration/register.html', {'form': form})
+
 
 @login_required
 def login_redirect(request):
     if request.user.is_authenticated():
-        return HttpResponseRedirect('/%s'%request.user.username )
+        return HttpResponseRedirect('/%s' % request.user.id )
     return HttpResponseRedirect('/')
+
 
 def maintenance(request):
     """Site is down for maintenance, display this view for all."""
     ctx = Context({})
-    
+
     return render_to_response('maintenance.html', context_instance=ctx)
+
 
 def live(request):
     """Render the live view."""
     game = Game.active_or_latest()
-    
+
     ctx = Context({
         'game': game,
         'user': request.user,
         'MEDIA_URL': settings.MEDIA_URL,
         'STATIC_URL': settings.STATIC_URL})
-    
+
     return render_to_response('live/index.html', context_instance=ctx)
