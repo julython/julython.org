5039aa829a5bb2e994e4cd8a5402ff1538801a65
Roman Levin <roman.levin@stxnext.pl>
2013-06-11 11:05:08 +0200

    Removed unused loadNpmTasks.

--- assets/Gruntfile.js
+++ assets/Gruntfile.js
@@ -99,7 +99,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 };
