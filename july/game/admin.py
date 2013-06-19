fd02ca0f4b77004f24bc105ad30f5d75270d4958
Roman Levin <roman.levin@stxnext.pl>
2013-06-19 10:48:50 +0200

    Adding language features to admin boards.

--- july/game/admin.py
+++ july/game/admin.py
@@ -1,19 +1,28 @@
 
 from django.contrib import admin
 
-from july.game.models import Game, Player, Board
+from july.game.models import Game, Player, Board, LanguageBoard
+
 
 class GameAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'start', 'end']
+
 
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ['user', 'game', 'points']
     list_filter = ['game']
 
+
 class BoardAdmin(admin.ModelAdmin):
     list_display = ['project', 'game', 'points']
+    list_filter = ['game']
+
+
+class LanguageBoardAdmin(admin.ModelAdmin):
+    list_display = ['language', 'game', 'points']
     list_filter = ['game']
 
 admin.site.register(Game, GameAdmin)
 admin.site.register(Player, PlayerAdmin)
 admin.site.register(Board, BoardAdmin)
+admin.site.register(LanguageBoard, LanguageBoardAdmin)
--- july/people/admin.py
+++ july/people/admin.py
@@ -1,21 +1,34 @@
 from django.contrib import admin
+from models import Team, Commit, Location, AchievedBadge
+from models import Badge, Language, Extension
 
-from models import Team, Commit, Location, Project, AchievedBadge, Badge
 
-admin.site.register(Commit, 
-    list_display=['hash', 'email', 'timestamp', 'project', 'user'], 
-    search_fields=['hash', 'email', 'project__name', 'user__username'], 
-    ordering=['-timestamp']
-)
-admin.site.register(Location, 
+class ExtensionInline(admin.TabularInline):
+    model = Extension
+
+admin.site.register(
+    Commit,
+    list_display=['hash', 'email', 'timestamp', 'project', 'user'],
+    search_fields=['hash', 'email', 'project__name', 'user__username'],
+    ordering=['-timestamp'])
+
+admin.site.register(
+    Location,
     list_display=["__unicode__", "slug", 'total'],
-    ordering=['-total']
-)
-admin.site.register(Team, 
+    ordering=['-total'])
+
+admin.site.register(
+    Language,
+    list_display=["__unicode__"],
+    inlines=[ExtensionInline])
+
+admin.site.register(
+    Team,
     list_display=["__unicode__", "slug", 'total'],
-    ordering=['-total']
-)
+    ordering=['-total'])
+
+admin.site.register(
+    Badge,
+    list_display=["__unicode__"])
+
 admin.site.register(AchievedBadge)
-admin.site.register(Badge,
-    list_display=["__unicode__"]
-)
