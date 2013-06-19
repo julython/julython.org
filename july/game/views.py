ec5799544109b9ea06e007ae5b0458d7c61069f3
Roman Levin <roman.levin@stxnext.pl>
2013-06-19 12:46:44 +0200

    Routing view and draft templates for language stuff.

--- july/game/views.py
+++ july/game/views.py
@@ -1,55 +1,60 @@
 import datetime
 
-from django.http import Http404
 from django.views.generic import list, detail
 
-from july.game.models import Player, Game, Board
-from july.people.models import Project, Location, Team
+from july.game.models import Player, Game, Board, LanguageBoard
+from july.people.models import Project, Location, Team, Language
 
 
 class GameMixin(object):
-    
+    model = None
+
     def get_game(self):
         year = int(self.kwargs.get('year', 0))
         month = int(self.kwargs.get('month', 0))
         if not year:
-            game = Game.active()           
+            game = Game.active()
         else:
             date = datetime.datetime(year=year, month=month, day=15)
             game = Game.active(now=date)
         if game is None:
             game = Game.objects.latest()
         return game
-        
+
+    def get_queryset(self):
+        game = self.get_game()
+        return self.model.objects.filter(game=game).select_related()
+
 
 class PlayerList(list.ListView, GameMixin):
     model = Player
     paginate_by = 100
-    
-    def get_queryset(self):
-        game = self.get_game()
-        return Player.objects.filter(game=game).select_related()
 
 
 class BoardList(list.ListView, GameMixin):
     model = Board
     paginate_by = 100
-    
-    def get_queryset(self):
-        game = self.get_game()
-        return Board.objects.filter(game=game).select_related()
+
+
+class LanguageBoardList(list.ListView, GameMixin):
+    model = LanguageBoard
+    paginate_by = 100
 
 
 class ProjectView(detail.DetailView):
     model = Project
-    
+
+
+class LanguageView(detail.DetailView):
+    model = Language
 
 class LocationCollection(list.ListView, GameMixin):
     model = Location
-    
+
     def get_queryset(self):
         game = self.get_game()
         return game.locations
+
 
 class LocationView(detail.DetailView):
     model = Location
@@ -57,10 +62,11 @@ class LocationView(detail.DetailView):
 
 class TeamCollection(list.ListView, GameMixin):
     model = Team
-    
+
     def get_queryset(self):
         game = self.get_game()
         return game.teams
+
 
 class TeamView(detail.DetailView):
     model = Team
