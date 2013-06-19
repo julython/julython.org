e169b204b1e99192b2e97cc34cf2da0b984463ff
Roman Levin <roman.levin@stxnext.pl>
2013-06-19 10:47:55 +0200

    Migrations for new Language models.

---
+++ july/people/migrations/0005_auto__add_language__add_extension.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Language'
+        db.create_table(u'people_language', (
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=64, primary_key=True)),
+        ))
+        db.send_create_signal(u'people', ['Language'])
+
+        # Adding model 'Extension'
+        db.create_table(u'people_extension', (
+            ('extension', self.gf('django.db.models.fields.SlugField')(max_length=10, primary_key=True)),
+            ('language', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['people.Language'])),
+        ))
+        db.send_create_signal(u'people', ['Extension'])
+
+        # Adding M2M table for field languages on 'Project'
+        m2m_table_name = db.shorten_name(u'people_project_languages')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('project', models.ForeignKey(orm[u'people.project'], null=False)),
+            ('language', models.ForeignKey(orm[u'people.language'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['project_id', 'language_id'])
+
+        # Adding M2M table for field languages on 'Commit'
+        m2m_table_name = db.shorten_name(u'people_commit_languages')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('commit', models.ForeignKey(orm[u'people.commit'], null=False)),
+            ('language', models.ForeignKey(orm[u'people.language'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['commit_id', 'language_id'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Language'
+        db.delete_table(u'people_language')
+
+        # Deleting model 'Extension'
+        db.delete_table(u'people_extension')
+
+        # Removing M2M table for field languages on 'Project'
+        db.delete_table(db.shorten_name(u'people_project_languages'))
+
+        # Removing M2M table for field languages on 'Commit'
+        db.delete_table(db.shorten_name(u'people_commit_languages'))
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'july.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'location': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'location_members'", 'null': 'True', 'to': u"orm['people.Location']"}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'picture_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'projects': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['people.Project']", 'null': 'True', 'blank': 'True'}),
+            'team': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'team_members'", 'null': 'True', 'to': u"orm['people.Team']"}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'people.achievedbadge': {
+            'Meta': {'object_name': 'AchievedBadge'},
+            'achieved_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'badge': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['people.Badge']", 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['july.User']", 'null': 'True', 'blank': 'True'})
+        },
+        u'people.badge': {
+            'Meta': {'object_name': 'Badge'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '2024', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'text': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        u'people.commit': {
+            'Meta': {'ordering': "['-timestamp']", 'object_name': 'Commit'},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'hash': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'languages': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['people.Language']", 'symmetrical': 'False', 'blank': 'True'}),
+            'message': ('django.db.models.fields.CharField', [], {'max_length': '2024', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['people.Project']", 'null': 'True', 'blank': 'True'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '512', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['july.User']", 'null': 'True', 'blank': 'True'})
+        },
+        u'people.extension': {
+            'Meta': {'object_name': 'Extension'},
+            'extension': ('django.db.models.fields.SlugField', [], {'max_length': '10', 'primary_key': 'True'}),
+            'language': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['people.Language']"})
+        },
+        u'people.language': {
+            'Meta': {'object_name': 'Language'},
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64', 'primary_key': 'True'})
+        },
+        u'people.location': {
+            'Meta': {'object_name': 'Location'},
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'primary_key': 'True'}),
+            'total': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        u'people.project': {
+            'Meta': {'object_name': 'Project'},
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'forked': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'forks': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'languages': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['people.Language']", 'symmetrical': 'False', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'parent_url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'repo_id': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'service': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '30', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'watchers': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        u'people.team': {
+            'Meta': {'object_name': 'Team'},
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'primary_key': 'True'}),
+            'total': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        }
+    }
+
+    complete_apps = ['people']
