2c65def281e80bfc667f313832960f3bb676be2b
Roman Levin <roman.levin@stxnext.pl>
2013-06-24 13:08:40 +0200

    Fixing jasmine test options.

--- assets/Gruntfile.js
+++ assets/Gruntfile.js
@@ -87,10 +87,12 @@ module.exports = function(grunt) {
 
     // Run our Jasmine tests
     jasmine: {
-      src: deps.concat(apps),
-      specs: 'js/spec/**/*Spec.js',
-      junit: {
-        output: 'junit/'
+      julython: {
+        src: deps.concat(apps),
+        specs: 'js/spec/**/*Spec.js',
+        junit: {
+          output: 'junit/'
+        }
       }
     }
 
